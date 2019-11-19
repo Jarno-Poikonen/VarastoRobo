8816d83f9243dd7d0d628632dba6afd435df2bf9
AnttiPyh <t5pyan00@students.oamk.fi>
2019-11-19 11:08:09 +0200

    Lisätty MPM toiminnallisuus.
    Lisätty tuki Move Prroduct Message viestille.
    Poistettu vanha gateway versio.
    Lisätty hätäseis toiminnallisuus.

--- UR5/Gateway/gateway_2.py
+++ UR5/Gateway/gateway_2.py
@@ -3,8 +3,9 @@ import socket
 from time import sleep
 import threading
 
-MasterIP = ""
+MasterIP = None
 MasterPort = None
+seis = False
 
 # Mahdollista tehdä paremmin.
 # Muutetaan viestin pituus esitettäväksi neljällä bytellä
@@ -44,15 +45,24 @@ def Broadcast_communication():
 			MasterIP, MasterPort = master_address
 			MasterPort = 1739
 	
+	# Luodaan socket hätäseis toiminnallisuutta varten
+	SEISsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	SEISsocket.bind(("192.168.100.11", 30001))
+	SEISsocket.listen()
+	
 	# Luetaan broadcast viestejä ja reagoidaan hätäseis käskyyn.
-	# while True:
-		# message, master_address = bSocket.recvfrom(512)
+	while True:
+		message, master_address = bSocket.recvfrom(512)
 		
-		# if len(message) >= 8 and message[0] == 1 and message[1] == 7 :
-			# system_status = int(message[2])
-			# if system_status != 1:
-				# seis = True
-				# break
+		if len(message) >= 8 and message[0] == 1 and message[1] == 7 :
+			system_status = int(message[2])
+			if system_status != 1:
+				global seis
+				seis = True
+				SEISyhteys, SEISosoite = SEISsocket.accept()
+				SEISyhteys.sendall("seis")
+				SEISyhteys.close()
+				break
 
 def Luo_NCM(tyyppi, id, tila):
 	viesti = bytearray([2])
@@ -84,10 +94,11 @@ def Luo_WFM(vastaukseksi, virhe, suoritus, tila):
 if __name__ == "__main__":
 
 	# Käsien paikat.
+	# Ensimämisenä tuotteen numero, sen jälkeen 6 float arvoa servoille.
 	paikat = [
 	#str.encode("(-0.586169,-0.413107,0.242636,-1.91015,-1.85181,0.584344)"),
-	str.encode("(-0.586169,-0.413107,0.242636,-1.91015,-1.85181,0.584344)"),
-	str.encode("(5.89284,-2.85641,-1.68474,-0.149206,1.56736,-0.316181)")
+	str.encode(",-0.586169,-0.413107,0.242636,-1.91015,-1.85181,0.584344)"),
+	str.encode(",5.89284,-2.85641,-1.68474,-0.149206,1.56736,-0.316181)")
 	]
 	
 	broadcast_saie = threading.Thread(target=Broadcast_communication, args=())
@@ -95,17 +106,17 @@ if __name__ == "__main__":
 	
 	ID = 55
 	
-	# URIP = ""
-	# URPort = 0
+	URIP = "192.168.100.11"
+	URPort = 30000
 	
-	# URSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# URSocket.bind((URIP, URPort))
-	# URSocket.listen()
+	URSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	URSocket.bind((URIP, URPort))
+	URSocket.listen()
 	
-	# URYhteys, URAddr = URSocket.accept()
+	URYhteys, URAddr = URSocket.accept()
 	
 	print("Odotetaan Masterin IP.")
-	while MasterPort is None:
+	while MasterIP is None:
 		pass
 	
 	print("IP saatu.")
@@ -115,15 +126,32 @@ if __name__ == "__main__":
 	print(viesti)
 	MasterSocket.send(viesti)
 	sleep(1)
-	while True:
+	while not seis:
 		MasterData = MasterSocket.recv(512)
 		print(MasterData)
 		try:
-			if MasterData[0] == 3:
+			if MasterData[0] == 3: # Setup Connection Message
+				print("Setup Connection Message saatu")
 				MasterSocket.sendall(Luo_WFM(MasterData[0], 0, 1, 1))
-				break
-		except:
-			sleep(5)
+				# break
+				continue
+			if MasterData[0] == 12: # Move Product Message
+				print("Move Product Message saatu")
+				data = str.encode("(" + MasterData[5] + paikat[MasterData[6]])
+				URYhteys.sendall(data)
+				
+				URData = URYhteys.recv(512)
+				
+				print(URData)
+				
+				if URData == "Valmis":
+					MasterSocket.sendall(Luo_WFM(MasterData[0], 0, 1, 1))
+				elif URData == "Fail":
+					MasterSocket.sendall(Luo_WFM(MasterData[0], 4, 1, 1))
+				
+				continue
+		except IndexError:
+			sleep(1)
 			continue
 	
 	print("Valmis")
