5168afab3823810c2020ae4b98a98d033afaded3
Joonas Mustonen <45063533+t7mujo01@users.noreply.github.com>
2019-11-26 13:55:23 +0200

    Add files via upload
    Some minor changes

--- GoPiGo3/PythonClient/gopigo_client.py
+++ GoPiGo3/PythonClient/gopigo_client.py
@@ -2,9 +2,10 @@
 import socket
 import time
 import threading
+import gpgDrive
 
 masterIp = None
-systemStop = False
+
 deviceID = None
 
 def count_length(message, length=4):   
@@ -17,7 +18,6 @@ def count_length(message, length=4):
     return returnVar
 
 def udp_broadcast():
-    #Thread for broadcast messages
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
     sock.bind(("", 1732))
@@ -35,9 +35,6 @@ def udp_broadcast():
                 masterIp = str(masterAddress[0])
                 print(masterIp)
                 waitMessage = False
-            
-            #else:
-                #systemStop = True
 
 def form_NCM(devType,devID,cordX,cordY,direction,devState):
     message = bytearray([2])
@@ -52,9 +49,9 @@ def form_NCM(devType,devID,cordX,cordY,direction,devState):
         
     return message
 
-def form_WFM(commandNum,errorCode,atom,cordX,cordY,direction,deviceState):
+def form_WFM(commandNum,errorCode,atom,cordX,cordY,direction,deviceState,packetNum):
     message = bytearray([4])
-    mesEndPart = bytearray([commandNum,errorCode,atom,cordX,cordY,direction,deviceState])
+    mesEndPart = bytearray([commandNum,errorCode,atom,cordX,cordY,direction,deviceState,packetNum])
     length = count_length(len(mesEndPart))
     
     for i in length:
@@ -64,51 +61,62 @@ def form_WFM(commandNum,errorCode,atom,cordX,cordY,direction,deviceState):
         message.append(i)
         
     return message
-
     
 def main():
+    systemStop = 0
     broadcast_thread = threading.Thread(target=udp_broadcast,args=())
     broadcast_thread.start()
 
     while masterIp is None:
-        print("ei IP:ta")
+        #print("ei IP:ta")
         #time.sleep(1)
         pass
     
-    print("Ip saatu")
+    #print("Ip saatu")
     masterSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     masterSocket.connect((masterIp, 1739))
-    print("Yhteys muodostettu")
+    #print("Yhteys muodostettu")
     message = form_NCM(2,255,0,0,0,1)
     masterSocket.send(message)
     
-    while not systemStop:
+    while systemStop == 0:
         fromMaster = masterSocket.recv(512)
         print("Masterin vastaus",fromMaster)
         
         #SCM
         if fromMaster[0] == 3:
             deviceID = fromMaster[6]
-            print(deviceID)
-            reply = form_WFM(3,0,1,0,0,0,1)
+            #print(deviceID)
+            pos = gpgDrive2.PosOri()
+            print(pos[0])
+            reply = form_WFM(3,0,1,pos[0],pos[1],0,1,0)
             masterSocket.send(reply)
-            print("Vastattu SCM")
+            print(reply)
+            print("SCM vastattu")
+            #risteys = gpgDrive.Liiku(4)
+            #print(risteys)
         
         #CCM
-        #if fromMaster[0] == 5:
-            #reply = form_WFM(5,0,1,0,0,0,1)
-            #masterSocket.send(reply)
+        if fromMaster[0] == 5:
+            print("Vastaanotettu CCM")
+            reply = form_WFM(5,0,1,0,0,0,1,0)
+            masterSocket.send(reply)
+            systemStop = 1
             
         #SQM    
-        #if fromMaster[0] == 6:
-            #reply = form_WFM(6,0,1,0,0,0,1)
-            #masterSocket.send(reply)
+        if fromMaster[0] == 6:
+            reply = form_WFM(6,0,1,0,0,0,1,0)
+            masterSocket.send(reply)
+            print("SQM vastattu")
+            
         
         #MCM
-        #if fromMaster[0] == 13:
-            #direction = fromMaster[5]
+        if fromMaster[0] == 13:
+            direction = fromMaster[5]
             
+    
+    masterSocket.close()
+    print("Yhteys katkaistiin")   
 
 if __name__ == "__main__":    
     main()
-            
