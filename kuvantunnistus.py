dde91a04d9bccdb3dd85df9067f9075c6b7a4dc2
Aleksi Mikkola <44567629+aleksimik@users.noreply.github.com>
2019-11-26 12:22:06 +0200

    Add files via upload
    Koodia kommentoitu

--- kuvantunnistus.py
+++ kuvantunnistus.py
@@ -1,29 +1,29 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) 
 
 font = cv2.FONT_HERSHEY_COMPLEX
 
 while True:
     _, frame = cap.read()
     
-    crop_frame = frame[140:400, 70:550]
+    crop_frame = frame[140:400, 70:550] #rajataan kuva-alue
     
-    hsv_frame = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2HSV)
+    hsv_frame = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2HSV) #muunnetaan BGR varit HSV vareiksi
     
-    low_red = np.array([161, 100, 0])
-    high_red = np.array([179, 255, 255])
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
-    red = cv2.bitwise_and(crop_frame, crop_frame, mask=red_mask)
+    low_red = np.array([161, 100, 0]) #aseteaan varin alaraja-arvo
+    high_red = np.array([179, 255, 255]) #asetetaan varin ylaraja-arvo
+    red_mask = cv2.inRange(hsv_frame, low_red, high_red) #etsitaan maaritelty vari kuvasta
+    red = cv2.bitwise_and(crop_frame, crop_frame, mask=red_mask) #poistetaan mut paitsi haluttu vari kuvasta
     
-    gray = cv2.cvtColor(red, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.threshold(blurred, 20, 255, cv2.THRESH_BINARY)[1]
-    kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.erode(thresh, kernel)
+    gray = cv2.cvtColor(red, cv2.COLOR_BGR2GRAY) #muutetaan varit harmaaksi
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0) #sumennetaan kuvaa
+    thresh = cv2.threshold(blurred, 20, 255, cv2.THRESH_BINARY)[1] #luodaan mustavalko kuva raja-arvon ylittavista alueista
+    kernel = np.ones((5, 5), np.uint8) #luodaan ykkosilla taytetty array
+    mask = cv2.erode(thresh, kernel) #kutistetaan kohdetta
     
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #etsitaan muodot
     
     for cnt in contours:
         area = cv2.contourArea(cnt)
@@ -31,11 +31,12 @@ while True:
         x = approx.ravel()[0]
         y = approx.ravel()[1]
         
-        if area > 400:
-            cv2.drawContours(crop_frame, [approx], 0, (0, 255, 0), 2)
+        if area > 400: #maaritetaan muotojen minimikoko
+            cv2.drawContours(crop_frame, [approx], 0, (0, 255, 0), 2) #piirretaan muodon aariviivat kuvaan ja maaritellaan sen vari
             
+            #tunnisteaan muodot
             if len(approx) == 3:
-                cv2.putText(crop_frame, "Triangle", (x, y), font, 1, (0, 255, 0))
+                cv2.putText(crop_frame, "Triangle", (x, y), font, 1, (0, 255, 0)) #lisataan teksti ruudulle ja maaritellaan sen vari
                 shape = "Triangle"
                 print(shape)
             elif len(approx) == 4:
@@ -50,14 +51,16 @@ while True:
                 cv2.putText(crop_frame, "Circle", (x, y), font, 1, (0, 255, 0))
                 shape = "Circle"
                 print(shape)
-       
-    #cv2.imshow("Frame", crop_frame)
+      
+    #avataan kameran kuvaa nayttava ikkuna 
+    cv2.imshow("Frame", crop_frame)
     #cv2.imshow("Red", red)
     #cv2.imshow("Gray", gray)
     #cv2.imshow("Blurred", blurred)
     #cv2.imshow("Thresh", thresh)
-    #cv2.imshow("Mask", mask)
+    cv2.imshow("Mask", mask)
     
+    #odotetaan esc nappaimen painamista scriptin lopettamiseksi
     key = cv2.waitKey(1)
     if key == 27:
         break
