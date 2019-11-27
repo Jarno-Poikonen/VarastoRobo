0978a897e345512d61f6b38b997a8f6d25a57c67
Aleksi Mikkola <44567629+aleksimik@users.noreply.github.com>
2019-11-27 10:07:43 +0200

    Add files via upload
    Poistettu turhaa koodia

--- kuvantunnistus.py
+++ kuvantunnistus.py
@@ -16,12 +16,8 @@ while True:
     high_red = np.array([179, 255, 255]) #asetetaan varin ylaraja-arvo
     red_mask = cv2.inRange(hsv_frame, low_red, high_red) #etsitaan maaritelty vari kuvasta
     red = cv2.bitwise_and(crop_frame, crop_frame, mask=red_mask) #poistetaan muut paitsi haluttu vari kuvasta
-    
-    gray = cv2.cvtColor(red, cv2.COLOR_BGR2GRAY) #muutetaan varit harmaaksi
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0) #sumennetaan kuvaa
-    thresh = cv2.threshold(blurred, 20, 255, cv2.THRESH_BINARY)[1] #luodaan mustavalko kuva raja-arvon ylittavista alueista
     kernel = np.ones((5, 5), np.uint8) #luodaan ykkosilla taytetty array
-    mask = cv2.erode(thresh, kernel) #kutistetaan kohdetta
+    mask = cv2.erode(red_mask, kernel) #kutistetaan kohdetta
     
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #etsitaan muodot
     
@@ -55,10 +51,8 @@ while True:
     #avataan kameran kuvaa nayttava ikkuna 
     #cv2.imshow("Frame", crop_frame)
     #cv2.imshow("Red", red)
-    #cv2.imshow("Gray", gray)
-    #cv2.imshow("Blurred", blurred)
-    #cv2.imshow("Thresh", thresh)
     #cv2.imshow("Mask", mask)
+    #cv2.imshow("Red mask", red_mask)
     
     #odotetaan esc nappaimen painamista scriptin lopettamiseksi
     key = cv2.waitKey(1)
