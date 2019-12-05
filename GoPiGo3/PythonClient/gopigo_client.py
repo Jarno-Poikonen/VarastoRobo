8eaa3deb44d812d226752fd7ce0bc8cc234c5ccb
Joonas Mustonen <45063533+t7mujo01@users.noreply.github.com>
2019-12-05 09:02:44 +0200

    Add files via upload
    Alustava hätäseis ja kuvantunnistus lisätty

--- GoPiGo3/PythonClient/gopigo_client.py
+++ GoPiGo3/PythonClient/gopigo_client.py
@@ -2,13 +2,14 @@
 import socket
 import time
 import threading
-import Clientohjattu
+import ClientControlled
 import kuvantunnistus
 
-masterIp = None 
-deviceID = None
+masterIp = None
+emergencyStop = False
 no_packet = 255
 
+#Listens SBM and emergency stop messages
 def udp_broadcast():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
@@ -16,24 +17,36 @@ def udp_broadcast():
     waitMessage = True
     while waitMessage == True:
         message, masterAddress = sock.recvfrom(512)
-        message = bytearray(message)
-        
-        #SBM
+        message = bytearray(message) 
+
         if len(message) >= 8 and message[0] == 1 and message[1] == 7:
-            masterId = message[3]
-            
+            masterId = message[3]           
             if masterId != 0:
                 global masterIp
                 masterIp = str(masterAddress[0])
                 print(masterIp)
                 waitMessage = False
+        
+    while True:
+        message, masterAddress = sock.recvfrom(512)
+        message = bytearray(message)
+        
+        if len(message) >= 8 and message[0] == 1 and message[1] == 7:
+            masterId = message[3]
+            if masterId == 0 and message[2] != 1:
+                global emergencyStop
+                #print("Hataseis!!")
+                emergencyStop = True
+                while True:
+                    ClientControlled.gpg.stop()
 
 def form_NCM(devType,devID,cordX,cordY,direction,devState):
     content_length = 6
+    
     message = bytearray([2])
     message.extend(bytearray((content_length).to_bytes(4, 'little')))
     message.extend(bytearray([devType,devID,cordX,cordY,direction,devState]))
-        
+    
     return message
 
 def form_WFM(commandNum,errorCode,atom,cordX,cordY,direction,devState,packetNum):
@@ -48,9 +61,9 @@ def form_WFM(commandNum,errorCode,atom,cordX,cordY,direction,devState,packetNum)
         message.append(packetNum)
         
     return message
-    
+
 def main():
-    systemStop = 0
+    disconnect = 0
     deviceState = 0
     latestDir = 0
     errorCode = 0
@@ -60,65 +73,65 @@ def main():
     broadcast_thread.start()
 
     while masterIp is None:
-        #print("ei IP:ta")
-        #time.sleep(1)
         pass
     
-    #print("Ip saatu")
     masterSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     masterSocket.connect((masterIp, 1739))
-    #print("Yhteys muodostettu")
-    #systemStatus = 1
-    message = form_NCM(2,255,0,0,0,deviceState)
-    #print(message)
+    pos, orientation = ClientControlled.PosOri()
+    message = form_NCM(2,255,pos[0],pos[1],orientation,deviceState)
     masterSocket.send(message)
     
-    while systemStop == 0:
+    while disconnect == 0 and emergencyStop == False:
         fromMaster = masterSocket.recv(512)
-        print("Masterin vastaus",fromMaster)
+        #print("Masterin vastaus",fromMaster)
 
         #SCM
         if fromMaster[0] == 3:
             deviceID = fromMaster[6]
             deviceState = 1
-            pos, orientation = Clientohjattu.PosOri()
+            pos, orientation = ClientControlled.PosOri()
+            carriedPacket = kuvantunnistus.kuvantunnistus()
             reply = form_WFM(fromMaster[0],0,1,pos[0],pos[1],orientation,deviceState,carriedPacket)
             masterSocket.send(reply)
-            #print(reply)
             print("SCM vastattu")
 
         #CCM
         elif fromMaster[0] == 5:
             print("Vastaanotettu CCM")
-            pos, orientation = Clientohjattu.PosOri()
+            pos, orientation = ClientControlled.PosOri()
+            carriedPacket = kuvantunnistus.kuvantunnistus()
             reply = form_WFM(fromMaster[0],0,1,pos[0],pos[1],orientation,deviceState,carriedPacket)
             masterSocket.send(reply)
-            systemStop = 1
+            disconnect = 1
 
         #SQM    
         elif fromMaster[0] == 6:
-            pos, orientation = Clientohjattu.PosOri()
+            pos, orientation = ClientControlled.PosOri()
+            carriedPacket = kuvantunnistus.kuvantunnistus()
             reply = form_WFM(fromMaster[0],0,1,pos[0],pos[1],orientation,deviceState,carriedPacket)
             masterSocket.send(reply)
             print("SQM vastattu")
 
         #MCM
         elif fromMaster[0] == 13 and deviceState == 1:
+            print("MCM vastaanotettu")
             direction = fromMaster[5]
-            errorCode = Clientohjattu.Liiku(direction) 
-            pos, orientation = Clientohjattu.PosOri()
+            errorCode = ClientControlled.Move(direction)
+            pos, orientation = ClientControlled.PosOri()
             carriedPacket = kuvantunnistus.kuvantunnistus()
             reply = form_WFM(fromMaster[0],errorCode,1,pos[0],pos[1],orientation,deviceState,carriedPacket)
             masterSocket.send(reply)
-            
+        
+        #Komentoa ei tunnistettu
         else:
-            print("Komentoa ei tueta/tunnistettu")
-            pos, orientation = Clientohjattu.PosOri()
-            reply = form_WFM(fromMaster[0],0,1,pos[0],pos[1],orientation,deviceState,carriedPacket)
+            print("Komentoa ei tueta")
+            pos, orientation = ClientControlled.PosOri()
+            carriedPacket = kuvantunnistus.kuvantunnistus()
+            reply = form_WFM(fromMaster[0],2,1,pos[0],pos[1],orientation,deviceState,carriedPacket)
             masterSocket.send(reply)
     
     masterSocket.close()
-    print("Yhteys katkaistiin")   
+    print("Yhteys katkaistu")
 
 if __name__ == "__main__":    
     main()
